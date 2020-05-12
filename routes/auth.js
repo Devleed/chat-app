af116575ea10019dbab104251478d4da2b1d581d
Waleed <mujahidwaleed11@gmail.com>
2020-05-13 01:14:06 +0500

    updated

--- routes/auth.js
+++ routes/auth.js
@@ -25,7 +25,7 @@ router.get(
  * google kick callback on this route and then we handle on our server
  */
 router.get('/google/callback', passport.authenticate('google'), (req, res) => {
-  res.redirect('localhost:3000');
+  res.json({ user: req.user });
 });
 
 /**
