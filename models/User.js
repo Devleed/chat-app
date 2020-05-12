cba8afe72cc5d737081dceba83dd69a69053d0f9
Waleed <mujahidwaleed11@gmail.com>
2020-05-12 18:41:01 +0500

    bugs-fixed

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   googleId: String,
   displayName: String,
-  username: { type: String, default: '' }
+  username: String
 });
 
 module.exports = User = mongoose.model('user', userSchema);
