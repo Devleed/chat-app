188d002424561270932d20b72120edb80eca28b5
Waleed <mujahidwaleed11@gmail.com>
2020-05-15 03:20:17 +0500

    'bugs-fixed'

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const app = express();
 // enabling cors
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('auth-token', '*');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
